b2e1a8fe1df8e3c839f6f7d41c3325ec6aa12829
Thilo@home <thilo.kernchen@gmail.com>
2018-10-11 21:51:59 +0900

    no message

--- users/views.py
+++ users/views.py
@@ -22,29 +22,18 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, 
-<<<<<<< HEAD
                                    request.FILES, 
-=======
-                                   request.FILES,
->>>>>>> 5e137b64a1f67d8abd4f5a8e73b38b516b5b1b84
                                    instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-<<<<<<< HEAD
             messages.success(request, f'Your account has been updated!')
-=======
-            messages.success(request, f'Your account has been updated.')
->>>>>>> 5e137b64a1f67d8abd4f5a8e73b38b516b5b1b84
             return redirect('profile')
             
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-<<<<<<< HEAD
-=======
-    
->>>>>>> 5e137b64a1f67d8abd4f5a8e73b38b516b5b1b84
+
     context =  {
             'u_form': u_form,
             'p_form': p_form
